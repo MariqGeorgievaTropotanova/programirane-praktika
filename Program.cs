edfab257eae676cff2d2e6e3d4387dea7cd5b58c
MariqGeorgievaTropotanova <145642355+MariqGeorgievaTropotanova@users.noreply.github.com>
2023-09-21 08:47:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int[] numbers = input.Split().Select(int.Parse).ToArray();
+            double average = numbers.Average();
+            var filteredNumbers = numbers.Where(num => num > average);
+            var result = filteredNumbers.OrderByDescending(num => num).Take(5);
+
+            {
+
+                Console.WriteLine(string.Join(" ", result));
+            }
+        }
+    }
+}
